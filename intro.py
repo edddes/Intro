2f633fd9f4cebd374d685c7e99b316f6e77f7c2e
edddes <edde.ranheden@gmail.com>
2023-08-31 13:23:30 +0200

    first python

---
+++ intro.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+print("""
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum
+      Lorem Ipsum""")
+
+print("hello""\n hello world")
+
+
+
